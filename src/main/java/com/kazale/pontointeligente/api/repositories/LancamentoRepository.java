c9dc37475dc66786847ba4b38770ed6460cb7b49
unknown <fbentodsca@gmail.com>
2018-10-01 13:29:56 +0100

    Unitary test

--- src/main/java/com/kazale/pontointeligente/api/repositories/LancamentoRepository.java
+++ src/main/java/com/kazale/pontointeligente/api/repositories/LancamentoRepository.java
@@ -6,6 +6,8 @@ import org.hibernate.annotations.NamedQueries;
 import org.hibernate.annotations.NamedQuery;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +17,7 @@ import com.kazale.pontointeligente.api.entities.Lancamento;
 @NamedQueries ({
 		@NamedQuery(name = "LancamentoRepository.findByFuncionarioId",
 				query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionario.id = :funcionadioId")})
-public interface LancamentoRepository {
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
 	List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
-	Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
+	Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, PageRequest page);
 }
